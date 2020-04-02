633feaa02d5f22cccef92be96e9f9028efb4bbcf
Assem <asemsuper2001@mail.ru>
2020-04-02 22:32:41 +0600

    commit

--- Week9/webdevenv/Scripts/activate.bat
+++ Week9/webdevenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Nazerke\Documents\Web Dev\week9\webdevenv
+set VIRTUAL_ENV=C:\Users\Дакотти\Desktop\WEB\Week9\webdevenv
 
 if not defined PROMPT set PROMPT=$P$G
 
